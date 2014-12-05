df88494ff307187ecfdafeb37dd4b3bc4c64bdf0
Andy Shinsel <andy.shinsel@gmail.com>
2014-12-04 19:26:24 -0800

    Added servo class

---
+++ src/modules/tools/servo/Servo.cpp
@@ -0,0 +1,18 @@
+/*
+ * Servo.cpp
+ *
+ *  Created on: Nov 30, 2014
+ *      Author: heidi
+ */
+
+#include "Servo.h"
+
+Servo::Servo() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Servo::~Servo() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/modules/tools/servo/Servo.h
@@ -0,0 +1,17 @@
+/*
+ * Servo.h
+ *
+ *  Created on: Nov 30, 2014
+ *      Author: heidi
+ */
+
+#ifndef SRC_MODULES_TOOLS_SERVO_SERVO_H_
+#define SRC_MODULES_TOOLS_SERVO_SERVO_H_
+
+class Servo: public Module {
+public:
+	Servo();
+	virtual ~Servo();
+};
+
+#endif /* SRC_MODULES_TOOLS_SERVO_SERVO_H_ */
